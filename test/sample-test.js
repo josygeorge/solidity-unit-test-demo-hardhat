6316d037e80286e9a8969f9e611405958133440b
Josy George <joe.george.josy@gmail.com>
2022-06-27 20:18:06 -0400

    added beforeEach

--- test/sample-test.js
+++ test/sample-test.js
@@ -2,10 +2,19 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
+
+  let contract;
+  let owner;
+
+  beforeEach(async function () {
     const Greeter = await ethers.getContractFactory("Greeter");
     const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+    contract = await greeter.deployed();
+    [owner] = await ethers.getSigners();
+  });
+
+  /* it("Should return the new greeting once it's changed", async function () {
+
 
     expect(await greeter.greet()).to.equal("Hello, world!");
 
@@ -15,18 +24,13 @@ describe("Greeter", function () {
     await setGreetingTx.wait();
 
     expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
+  }); */
+
   it("Should return the sum of two numbers", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    const contract = await greeter.deployed();
     const testSumOfNums = await contract.addNum(2, 6);
     expect(testSumOfNums).to.equal(8);
   });
   it("Should return the difference of two numbers", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    const contract = await greeter.deployed();
     const testDiffOfNums = await contract.subtractNum(2, 6);
     expect(testDiffOfNums).to.equal(-4);
   });
